51b73a603c3843763b93673131f1066bddd28ede
Rezo <37451147+dischello@users.noreply.github.com>
2018-03-29 18:53:34 +0300

    Update isJobStartedByTimer.groovy

--- vars/isJobStartedByTimer.groovy
+++ vars/isJobStartedByTimer.groovy
@@ -2,6 +2,6 @@
 def call(){
   //def causes = currentBuild.rawBuild.getCauses()
   //echo "${causes}"
-  def specificCause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause) != null
+  def specificCause = currentBuild.rawBuild.getCause(hudson.triggers.TimerTrigger.TimerTriggerCause) != null
   echo "Triggered by user? ${specificCause}"
 }
