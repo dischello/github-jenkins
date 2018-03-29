ca59ffd8685bcf10b2fd014b89bd1160106219c2
Rezo <37451147+dischello@users.noreply.github.com>
2018-03-29 14:33:31 +0300

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,13 +3,8 @@ node {
   def buildLog = currentBuild.rawBuild.log
   echo "currentBuild.rawBuild.log"
   cause = "${buildLog}"
-  println buildlog
-  
+    echo "${buildlog}
+  echo cause
   }
 }
 
-@NonCPS
-def getLastBuildCause() {
-    def causes = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).properties
-    return causes
-}
