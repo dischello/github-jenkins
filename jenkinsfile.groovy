82b91dd4f5c8b91f7dd03ac687e0bede5e78ffc1
Rezo <37451147+dischello@users.noreply.github.com>
2018-03-29 14:10:36 +0300

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,21 +1,10 @@
 node {
   stage 'Stage Upload To Fabric'
-  printCausesRecursively()
+  getLastBuildCause()
 }
 
 @NonCPS
-def printCausesRecursively(cause) {
-     if (cause.class.toString().contains("UpstreamCause")) {
-         println "This job was caused by " + cause.toString()
-         for (upCause in cause.upstreamCauses) {
-             printCausesRecursively(upCause)
-         }
-     } else {
-         println "Root cause : " + cause.toString()
-     }
-}
-
-for (cause in manager.build.causes)
-{
-    printCausesRecursively(cause)
+def getLastBuildCause() {
+    def causes = currentBuild.causes[0]
+    return causes
 }
