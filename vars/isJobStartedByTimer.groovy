39a65a573fca3b21e6672de206029877ad604a4f
Rezo <37451147+dischello@users.noreply.github.com>
2018-03-29 18:12:02 +0300

    Update isJobStartedByTimer.groovy

--- vars/isJobStartedByTimer.groovy
+++ vars/isJobStartedByTimer.groovy
@@ -1,28 +1,5 @@
 @NonCPS
-def call() {
-//def startedByTimer = false
-println "Start of the lib"
-//def test = manager.build.causes[0].toString()
-def isStartedByUser = currentBuild.rawBuild.getCause(hudson.model.Cause$TimerTrigger.TimerTriggerCause).toString()
-        println isStartedByUser "{$isStartedByUser}"
-return isStartedByUser
-/*
-try {
-        def buildCauses = currentBuild.rawBuild.getCauses(hudson.model.Cause$UserIdCause)
-        for ( buildCause in buildCauses ) {
-            if (buildCause != null) {
-                def causeDescription = buildCause.getShortDescription()
-                echo "shortDescription: ${causeDescription}"
-                if (causeDescription.contains("Started by timer")) {
-                    startedByTimer = true
-                }
-            }
-        }
-    } catch(theError) {
-        echo "Error getting build cause"
-    }
-
-    return startedByTimer
-    */
-}
-
+def build = manager.build   // "manager" for Groovy Postbuild plugin, only
+def usercause=build.getCause(hudson.model.Cause$UserCause)
+def thename=usercause.userName
+println thename
