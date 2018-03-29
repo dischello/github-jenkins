1d2e28e7e4572a3d909231ac4385695afdc6467b
Rezo <37451147+dischello@users.noreply.github.com>
2018-03-29 18:15:19 +0300

    Update isJobStartedByTimer.groovy

--- vars/isJobStartedByTimer.groovy
+++ vars/isJobStartedByTimer.groovy
@@ -1,5 +1,7 @@
 @NonCPS
-def build = manager.build   // "manager" for Groovy Postbuild plugin, only
-def usercause=build.getCause(hudson.model.Cause$UserCause)
-def thename=usercause.userName
-println thename
+def call(){
+  def build = manager.build   // "manager" for Groovy Postbuild plugin, only
+  def usercause=build.getCause(hudson.model.Cause$UserCause)
+  def thename=usercause.userName
+  println thename
+}
