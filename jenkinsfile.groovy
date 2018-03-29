3ff4f37acd8b5e940b0a8a46259f10e329356af9
Rezo <37451147+dischello@users.noreply.github.com>
2018-03-29 14:39:45 +0300

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,11 +1,32 @@
-node {
-  stage('Stage Upload To Fabric'){
-    def buildLog = currentBuild.rawBuild.log
-    echo "currentBuild.rawBuild.log"
-    cause = "${buildLog}"
-    echo "${buildlog}"
-    echo cause
-    echo "${cause}"
+pipeline{
+  agent any
+  options {
+  timestamp()
+  }
+  stages{
+    stage ('Test::Build::Cause'){
+      echo isJobStartedByTimer()
+    }
   }
 }
+// check if the job was started by a timer
+@NonCPS
+def isJobStartedByTimer() {
+    def startedByTimer = false
+    try {
+        def buildCauses = currentBuild.rawBuild.getCauses()
+        for ( buildCause in buildCauses ) {
+            if (buildCause != null) {
+                def causeDescription = buildCause.getShortDescription()
+                echo "shortDescription: ${causeDescription}"
+                if (causeDescription.contains("Started by timer")) {
+                    startedByTimer = true
+                }
+            }
+        }
+    } catch(theError) {
+        echo "Error getting build cause"
+    }
 
+    return startedByTimer
+}
