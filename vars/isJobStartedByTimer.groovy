d28ca1eaeaa8a647fb974c11be62ea85db3e2b47
Rezo <37451147+dischello@users.noreply.github.com>
2018-03-29 19:33:20 +0300

    Update isJobStartedByTimer.groovy

--- vars/isJobStartedByTimer.groovy
+++ vars/isJobStartedByTimer.groovy
@@ -2,6 +2,6 @@
 def call(){
   //def causes = currentBuild.rawBuild.getCauses()
   //echo "${causes}"
-  def specificCause = currentBuild.rawBuild.getCause(hudson.triggers.TimerTrigger.TimerTriggerCause).toString() != null
+  def specificCause = currentBuild.rawBuild.getCause(hudson.triggers.TimerTrigger$TimerTriggerCause) != null
   echo "Cron trigger ${specificCause}"
 }
